c27853ce8886c2a580667c6089780f7193b9e3b3
Nika-Firada <nika.firada@gmail.com>
2021-12-25 14:48:21 +0400

    first part

---
+++ arrays.js
@@ -0,0 +1,16 @@
+//1. შექმენით 5 ელემენტიანი მასივი, რომლის თვითვეული ელემენტი არის რიცხვი,
+// console.log გამოიტანეთ ამ მასივის ყველა ელემენტის ჯამი.
+const example = [5, 25, 30, 20, 20];
+let total = 0;
+for(let i = 0; i < example.length; i++){
+    total = total + example[i];
+}
+console.log(`total is ${total}`);
+
+/*2. შექმენით 3 ელემენტიანი მასივი, რომლის თვითვეული ელემენტი არის ობიექტი
+რომელსაც აქვს შემდეგი ველები (properties): name, age, address.
+ */
+
+
+//3. console.log ში გამოიტანეთ 3 სტრინგი
+//4. დაწერეთ if / else statement
