da91224b1188b815e77fc4c664716dfbce788dab
Nika-Firada <nika.firada@gmail.com>
2021-12-25 22:28:11 +0400

    all done

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 //1. შექმენით 5 ელემენტიანი მასივი, რომლის თვითვეული ელემენტი არის რიცხვი,
 // console.log გამოიტანეთ ამ მასივის ყველა ელემენტის ჯამი.
+
+// პირველი ხერხი
 const example = [5, 25, 30, 20, 20];
 let total = 0;
 for(let i = 0; i < example.length; i++){
@@ -7,10 +9,23 @@ for(let i = 0; i < example.length; i++){
 }
 console.log(`total is ${total}`);
 
+// მეორე ხერხი...
+let total2 = example[0] + example[1] + example[2] + example[3] + example[4];
+console.log(total2);
+
 /*2. შექმენით 3 ელემენტიანი მასივი, რომლის თვითვეული ელემენტი არის ობიექტი
 რომელსაც აქვს შემდეგი ველები (properties): name, age, address.
  */
-
-
+const example2 = [
+    {name: "Nika", age: 23, addres:"leonidze street"},
+    {name:"Gio", age: 25, addres:"shartava street"},
+    {name:"Nino", age: 22, addres:"kostava street"}
+];
 //3. console.log ში გამოიტანეთ 3 სტრინგი
+console.log(`My name is ${example2[0].name}, my age is ${example2[0].age}, my addres is ${example2[0].addres}.`);
 //4. დაწერეთ if / else statement
+if(example2[0].age < 19){
+    console.log("I am a teenager");
+} else{
+    console.log("I am an adult");
+}
